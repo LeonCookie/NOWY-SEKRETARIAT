ffebbdf855fe88c90b1e0638804e3f75b2356b49
LeonCookie <leongdynia1105@gmail.com>
2022-01-06 21:03:55 +0100

    Insert dla Nauczyciela działa!

--- NOWYSekretariat/MainWindow.xaml.cs
+++ NOWYSekretariat/MainWindow.xaml.cs
@@ -151,9 +151,39 @@ namespace NOWYSekretariat
         
         private void buttonSendnauczyciel_Click(object sender, RoutedEventArgs e)
         {
-            if (!string.IsNullOrEmpty(textbox_nauczyciel_imie.Text) && !string.IsNullOrEmpty(textbox_nauczyciel_nazwisko.Text) && !string.IsNullOrEmpty(textbox_nauczyciel_Pesel.Text)&& !string.IsNullOrEmpty(DataZatrudnienia_Nauczyciel.Text) && textbox_nauczyciel_Pesel.Text.Length == 11) //wymagane pola nie sa puste
+            if (!string.IsNullOrEmpty(textbox_nauczyciel_imie.Text) && !string.IsNullOrEmpty(textbox_nauczyciel_nazwisko.Text) && !string.IsNullOrEmpty(textbox_nauczyciel_Pesel.Text) && !string.IsNullOrEmpty(DataZatrudnienia_Nauczyciel.Text) && textbox_nauczyciel_Pesel.Text.Length == 11) //wymagane pola nie sa puste
             {
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True";
+                con.Open();
 
+                // insert command 
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = @"INSERT INTO  [Table-Nauczyciel](Imie, DrugieImie, Nazwisko, NazwiskoPanienskie, ImieMatki, ImieOjca,  Pesel, Plec,Wychowawstwo,PrzedmiotyNauczane,JakieKlasyUczy,DataUrodzenia,DataZatrudnienia ) VALUES(@Imie, @DrugieImie, @Nazwisko, @NazwiskoPanienskie, @ImieMatki, @ImieOjca, @Pesel,@Plec,@Wychowawstwo,@PrzedmiotyNauczane,@JakieKlasyUczy,@DataUrodzenia,@DataZatrudnienia)";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@Imie", textbox_nauczyciel_imie.Text);
+                cmd.Parameters.AddWithValue("@DrugieImie", textbox_nauczyciel_drugieImie.Text);
+                cmd.Parameters.AddWithValue("@Nazwisko", textbox_nauczyciel_nazwisko.Text);
+                cmd.Parameters.AddWithValue("@NazwiskoPanienskie", textbox_nauczyciel_nazwiskoPanienskie.Text);
+                cmd.Parameters.AddWithValue("@ImieMatki", textbox_nauczyciel_ImieMatki.Text);
+                cmd.Parameters.AddWithValue("@ImieOjca", textbox_nauczyciel_ImieOjca.Text);
+                cmd.Parameters.AddWithValue("@DataUrodzenia", data_urodzenia_nauczyciel.Text);
+                cmd.Parameters.AddWithValue("@Pesel", textbox_nauczyciel_Pesel.Text);
+                cmd.Parameters.AddWithValue("@Wychowawstwo", combobox_nauczyciel_WychowanieKlasy.Text);
+                cmd.Parameters.AddWithValue("@Plec", combobox_nauczyciel_plec.Text);
+
+                string przed = list_nauczuciel_przedmiotyNauczane.ToString();
+                cmd.Parameters.AddWithValue("@PrzedmiotyNauczane",przed);
+                string Jakie =list_nauczuciel_jakieKlasy.ToString();
+                cmd.Parameters.AddWithValue("@JakieKlasyUczy",Jakie);
+                cmd.Parameters.AddWithValue("@DataZatrudnienia", DataZatrudnienia_Nauczyciel.Text);
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+
+                binddatagrip2();
             }
             else
             {
@@ -173,6 +203,7 @@ namespace NOWYSekretariat
 
             }
             */
+           
         }
 
 
@@ -809,6 +840,11 @@ namespace NOWYSekretariat
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Debug.WriteLine(data_urodzenia.Text);
+        }
+
+        private void dane_uczen_search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
         }
 
 
