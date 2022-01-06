54daeb0ab796dab6ad857bdef50fbc6753ed4740
LeonCookie <leongdynia1105@gmail.com>
2022-01-06 21:33:43 +0100

    insert done(with alerts and cleaning)

--- NOWYSekretariat/MainWindow.xaml.cs
+++ NOWYSekretariat/MainWindow.xaml.cs
@@ -124,6 +124,8 @@ namespace NOWYSekretariat
                 con.Close();
                 
                 binddatagrip();
+
+                //wyczyszczenie textbox po wyslaniu
                 textbox_uczen_imie.Text = String.Empty;
                 textbox_uczen_drugieImie.Text = String.Empty;
                 textbox_uczen_nazwisko.Text = String.Empty;
@@ -174,9 +176,9 @@ namespace NOWYSekretariat
                 cmd.Parameters.AddWithValue("@Wychowawstwo", combobox_nauczyciel_WychowanieKlasy.Text);
                 cmd.Parameters.AddWithValue("@Plec", combobox_nauczyciel_plec.Text);
 
-                string przed = list_nauczuciel_przedmiotyNauczane.ToString();
+                string przed = list_nauczuciel_przedmiotyNauczane.SelectedItem.ToString();
                 cmd.Parameters.AddWithValue("@PrzedmiotyNauczane",przed);
-                string Jakie =list_nauczuciel_jakieKlasy.ToString();
+                string Jakie = list_nauczuciel_jakieKlasy.SelectedItem.ToString();
                 cmd.Parameters.AddWithValue("@JakieKlasyUczy",Jakie);
                 cmd.Parameters.AddWithValue("@DataZatrudnienia", DataZatrudnienia_Nauczyciel.Text);
                 cmd.ExecuteNonQuery();
@@ -184,6 +186,25 @@ namespace NOWYSekretariat
                 con.Close();
 
                 binddatagrip2();
+                textbox_nauczyciel_imie.Text = String.Empty;
+                textbox_nauczyciel_drugieImie.Text = String.Empty;
+                textbox_nauczyciel_nazwisko.Text = String.Empty;
+                textbox_nauczyciel_nazwiskoPanienskie.Text = String.Empty;
+                textbox_nauczyciel_ImieMatki.Text = String.Empty;
+                textbox_nauczyciel_ImieOjca.Text = String.Empty;
+                data_urodzenia_nauczyciel.Text = String.Empty;
+                textbox_nauczyciel_Pesel.Text = String.Empty;
+                combobox_nauczyciel_WychowanieKlasy.Text = String.Empty;
+                combobox_nauczyciel_plec.Text = String.Empty;
+                list_nauczuciel_przedmiotyNauczane.UnselectAll();
+                list_nauczuciel_jakieKlasy.UnselectAll();
+                DataZatrudnienia_Nauczyciel.Text = String.Empty;
+
+
+
+
+
+                MessageBox.Show("Wysłano!");
             }
             else
             {
@@ -194,15 +215,61 @@ namespace NOWYSekretariat
 
         private void buttonSendobsluga_Click(object sender, RoutedEventArgs e)
         {
-            /*
-            if (!string.IsNullOrEmpty(textbox_uczen_imie.Text) && !string.IsNullOrEmpty(textbox_uczen_nazwisko.Text) && !string.IsNullOrEmpty(textbox_uczen_nazwisko.Text) && !string.IsNullOrEmpty(textbox_uczen_Pesel.Text) && !string.IsNullOrEmpty(combobox_uczen_klasa.Text) && textbox_uczen_Pesel.Text.Length == 11) //wymagane pola nie sa puste
+            
+            if (!string.IsNullOrEmpty(textbox_obsluga_imie.Text) && !string.IsNullOrEmpty(textbox_obsluga_nazwisko.Text) && !string.IsNullOrEmpty(textbox_obsluga_Pesel.Text) &&!string.IsNullOrEmpty(DataZatrudnienia_obsluga.Text) && textbox_obsluga_Pesel.Text.Length == 11) //wymagane pola nie sa puste
             {
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True";
+                con.Open();
+
+                // insert command 
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = @"INSERT INTO  [Table-Ubsluga](Imie, DrugieImie, Nazwisko, NazwiskoPanienskie, ImieMatki, ImieOjca,  Pesel, Plec,InformacjaEtat,OpisStanowiska,DataZatrudnienia ) VALUES(@Imie, @DrugieImie, @Nazwisko, @NazwiskoPanienskie, @ImieMatki, @ImieOjca, @Pesel,@Plec,@InformacjeEtat,@OpisStanowiska,@DataZatrudnienia)";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@Imie", textbox_obsluga_imie.Text);
+                cmd.Parameters.AddWithValue("@DrugieImie", textbox_obsluga_drugieImie.Text);
+                cmd.Parameters.AddWithValue("@Nazwisko", textbox_obsluga_nazwisko.Text);
+                cmd.Parameters.AddWithValue("@NazwiskoPanienskie", textbox_obsluga_nazwiskoPanienskie.Text);
+                cmd.Parameters.AddWithValue("@ImieMatki", textbox_obsluga_ImieMatki.Text);
+                cmd.Parameters.AddWithValue("@ImieOjca", textbox_obsluga_ImieOjca.Text);
+                cmd.Parameters.AddWithValue("@DataUrodzenia", data_urodzenia_obsluga.Text);
+                cmd.Parameters.AddWithValue("@Pesel", textbox_obsluga_Pesel.Text);
+                cmd.Parameters.AddWithValue("@Plec", combobox_obsluga_plec.Text);
+                cmd.Parameters.AddWithValue("@InformacjeEtat", combobox_obsluga_Etat.Text);
+                cmd.Parameters.AddWithValue("@OpisStanowiska", textbox_obsluga_OpisStanowiska.Text);
+                cmd.Parameters.AddWithValue("@DataZatrudnienia", DataZatrudnienia_obsluga.Text);
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+
+                binddatagrip3();
+                textbox_obsluga_imie.Text = String.Empty;
+                    textbox_obsluga_drugieImie.Text = String.Empty;
+                textbox_obsluga_nazwisko.Text = String.Empty;
+                textbox_obsluga_nazwiskoPanienskie.Text = String.Empty;
+                textbox_obsluga_ImieMatki.Text = String.Empty;
+                textbox_obsluga_ImieOjca.Text = String.Empty;
+                data_urodzenia_obsluga.Text = String.Empty;
+                textbox_obsluga_Pesel.Text = String.Empty;
+                combobox_obsluga_plec.Text = String.Empty;
+                combobox_obsluga_Etat.Text = String.Empty;
+                textbox_obsluga_OpisStanowiska.Text = String.Empty;
+                DataZatrudnienia_obsluga.Text = String.Empty;
+
+
+
+
+
+
+
             }
             else
             {
-
+                MessageBox.Show("Uzupełnij wymagane pola: Imie,Nazwisko,Pesel(musi posiadac 11 cyfr),DataZatrudnienia");
             }
-            */
+            
            
         }
 
